06ff91433dbb00dea9ff494e3d0b83bea1a48dca
GozaRuu <kais.sghari@gmail.com>
2018-10-04 23:37:35 +0100

    exported more complex dummy data and used it as props

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent'
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Js Joe Restaurant</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu />
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
+    render() {
+        return (
+            <div>
+            <Navbar dark color="primary">
+                <div className="container">
+                    <NavbarBrand href="/">Js Joe Restaurant</NavbarBrand>
+                </div>
+            </Navbar>
+            <Menu dishes={this.state.dishes}/>
+            </div>
+        );
+    }
 }
 
 export default App;
