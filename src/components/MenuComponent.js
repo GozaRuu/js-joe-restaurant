f6f3f9aea8be314dd23dd5ab2a2b9c25e684ec68
GozaRuu <kais.sghari@gmail.com>
2018-10-04 23:52:53 +0100

    added selected dish onclick display feature

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardBody, CardTitle, CardText } from 'reactstrap';
 
 class Menu extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            selectedDish: null
+        };
+        this.onDishSelect = this.onDishSelect.bind(this);
     }
+
+    onDishSelect(dish) {
+        this.setState({selectedDish: dish});
+    }
+
+    renderDish(dish) {
+        if(dish != null) {
+            return (
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+    }
+
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card>
+                    <Card onClick={() => this.onDishSelect(dish)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
@@ -22,6 +45,9 @@ class Menu extends Component {
             <div className="container">
                 <div className="row">
                     {menu}
+                </div>
+                <div className="row">
+                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
         );
