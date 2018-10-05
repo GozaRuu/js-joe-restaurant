bf0076b7d55307ffa658f9fcd7913a4aaf2584b6
GozaRuu <kais.sghari@gmail.com>
2018-10-05 21:08:42 +0100

    added initial structure to HomeComponent, imported data in MenuComponent and updated routing to HomeComponent

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,21 +7,35 @@ import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import Dishdetail from './DishdetailComponent'
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            dishes: DISHES
+            dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         };
     }
 
     render() {
+        const HomeComponentCreator = () => {
+            const dish = this.state.dishes.filter((dish) => dish.featured)[0];
+            const promotion = this.state.promotions.filter((promotion) => promotion.featured)[0];
+            const leader = this.state.leaders.filter((leader) => leader.featured)[0];
+            return (
+                <Home dish={dish} promotion={promotion} leader={leader} />
+            );
+        };
         return (
             <div>
                 <Header />
                     <Switch>
-                        <Route path="/home" component={Home} />
+                        <Route path="/home" component={HomeComponentCreator} />
                         <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
                         <Route exact path="/contactus" component={Contact} />
                         <Redirect to="/home" />
