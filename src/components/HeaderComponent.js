c3bf0bf3e66f12dc99ba314f69495897edad5959
GozaRuu <kais.sghari@gmail.com>
2018-10-06 12:42:58 +0100

    developed loging Uncontrolled form in HeaderComponent

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, Collapse, Jumbotron, Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, Collapse, Jumbotron, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
@@ -11,6 +11,7 @@ class Header extends Component {
         };
         this.toggleNav = this.toggleNav.bind(this);
         this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
     }
 
     toggleNav() {
@@ -23,6 +24,12 @@ class Header extends Component {
         this.setState({
             isModalOpen: !this.state.isModalOpen
         });
+    }
+
+    handleLogin(event){
+        this.toggleModal();
+        alert(`UserName: ${this.username.value} Password: ${this.password.value} Remember: ${this.rememberme.checked}`);
+        event.preventDefault();
     }
 
     render() {
@@ -80,7 +87,23 @@ class Header extends Component {
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                     <ModalHeader>Login</ModalHeader>
                     <ModalBody>
-
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">User Name</Label>
+                                <Input type="text" id="username" name="username" innerRef={(input) => this.username = input}/>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password" innerRef={(input) => this.password = input}/>
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="rememberme" innerRef={(input) => this.rememberme = input}/>
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" className="bg-primary">Login</Button>
+                        </Form>
                     </ModalBody>
                 </Modal>
             </React.Fragment>
