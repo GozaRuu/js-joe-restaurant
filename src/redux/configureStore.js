381b2a74d3d84c53f2a40018391a69f839f2136a
GozaRuu <kais.sghari@gmail.com>
2018-10-06 23:22:36 +0100

    added redux-thunk and redux-logger

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { dishes } from './reducers/dishes';
 import { comments } from './reducers/comments';
 import { promotions } from './reducers/promotions';
 import { leaders } from './reducers/leaders';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -11,7 +13,8 @@ export const ConfigureStore = () => {
             comments,
             promotions,
             leaders
-        })
+        }),
+        applyMiddleware(thunk, logger)
     );
     return store;
 };
