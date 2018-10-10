b90eddb03a432731099c9c6b847577a0effb66cd
GozaRuu <kais.sghari@gmail.com>
2018-10-10 21:23:08 +0100

    updated config/cors.config.js

--- express-mongoose-api/config/cors.config.js
+++ express-mongoose-api/config/cors.config.js
@@ -3,11 +3,9 @@ const cors = require('cors');
 const whitelist = ['http://localhost:3000', 'https://localhost:3443'];
 
 const corsOptionsDelegate = (req, cb) => {
-	let corsOptions;
+	let corsOptions = { origin: false };
 	if (whilelist.indexOf(req.header('Origin')) !== -1) {
 		corsOptions = { origin: true };
-	} else {
-		corsOptions = { origin: false };
 	}
 	cb(null, corsOptions);
 }
