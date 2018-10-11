e5e2a267db2f7acbc7f76894c40adbd25a437da5
GozaRuu <kais.sghari@gmail.com>
2018-10-11 08:35:57 +0100

    updtated /favorites driver with mongoose populate

--- express-mongoose-api/routes/favorites.js
+++ express-mongoose-api/routes/favorites.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const Leaders = require('../models/favorites');
+const Favorites = require('../models/favorites');
 const authenticate = require('../config/passport.config').verifyUser;
 const cors = require('../config/cors.config');
 
@@ -11,7 +11,9 @@ favoriteRouter.use(bodyParser.json());
 favoriteRouter.route('/')
 .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
 .get(cors.cors, (req,res,next) => {
-    Leaders.find({})
+    Favorites.find({})
+	.populate('user')
+	.populate('favorites')
 	.then((favorites) => {
 		res.statusCode = 200;
 		res.json(favorites);
@@ -19,18 +21,34 @@ favoriteRouter.route('/')
 	.catch((err) => next(err));
 })
 .post(cors.corsWithOptions, authenticate, (req, res, next) => {
-    Leaders.create(req.body)
+	Favorites.findOne({ user: req.user._id })
 	.then((favorite) => {
-		res.statusCode = 200;
-		res.json(favorite);
-	});
+		if (!favorite) {
+			Favorites.create({ user: req.user._id, favorites: [req.body]})
+			.then((favoriteCreated) => {
+				res.statusCode = 200;
+				res.json(favoriteCreated);
+			})
+			.catch((err) => next(err));
+			return;
+		}
+		Favorites.update({ user: req.user._id, favorites: [...favorite.favorites, req.body] })
+		.then((favoriteUpdated) => {
+			res.statusCode = 200;
+			res.json(favoriteUpdated);
+		})
+		.catch((err) => next(err));
+		return;
+
+	})
+	.catch((err) => next(err));
 })
 .put(cors.corsWithOptions, authenticate, (req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /favorites');
 })
 .delete(cors.corsWithOptions, authenticate, (req, res, next) => {
-    Leaders.remove({})
+    Favorites.remove({})
 	.then((response) => {
 		res.statusCode = 200;
 		res.json(response);
